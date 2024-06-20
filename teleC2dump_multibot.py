bb7abf22dda05ea75975777b93d145a35bea1fad
fareedfauzi <56353946+fareedfauzi@users.noreply.github.com>
2024-06-20 08:21:16 +0800

    Update teleC2dump_multibot.py
    - Add new option to do both brute-force and updates OR just update
    - Add function for dynamic addition of new bot tokens

--- teleC2dump_multibot.py
+++ teleC2dump_multibot.py
@@ -4,12 +4,13 @@ import time
 import threading
 import logging
 from logging.handlers import RotatingFileHandler
+from queue import Queue
 
 # Error handling logger. Max 1GB size of log.
 def setup_logger():
     h = RotatingFileHandler('teleC2dump.log', maxBytes=1*1024*1024*1024, backupCount=1)
     h.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s - %levelname%- %message%', datefmt='%Y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     h.setFormatter(formatter)
     logger = logging.getLogger()
     logger.setLevel(logging.ERROR)
@@ -90,6 +91,14 @@ def processing_botData(botkey, attacker_chat_id, chatid, mode):
     except Exception as e:
         logging.error(f'Error in processing_botData - Bot: {botkey} - Error: {e}')
 
+def process_new_bot_tokens(queue, mode):
+    while True:
+        if not queue.empty():
+            botkey, attacker_chat_id, chatid = queue.get()
+            thread = threading.Thread(target=processing_botData, args=(botkey, attacker_chat_id, chatid, mode))
+            thread.start()
+            thread.join()
+
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 4:
         print("Usage: python3 teleC2dump_multibot.py <config_file> [-mode updateonly]")
@@ -103,9 +112,9 @@ def main():
                 -h, --help       Show this help message and exit
 
                 Arguments:
-                config_file          Configuration file containing bot tokens, attacker chat IDs, and your chat ID.
-                                     Each line in the file should have the format: <BOT TOKEN ID> <ATTACKER CHAT ID> <YOUR CHAT ID>
-                -mode updateonly     Only perform updates, skip brute force. Use case is when you only want the latest messages.
+                config_file      Configuration file containing bot tokens, attacker chat IDs, and your chat ID.
+                                 Each line in the file should have the format: <BOT TOKEN ID> <ATTACKER CHAT ID> <YOUR CHAT ID>
+                -mode updateonly Only perform updates, skip brute force
 
                 Example of config file format:
                 7342309939:AAFSgOLG_25mu-QZE8M7bSPufJJNknYj1JY 467115391 7093036821
@@ -122,22 +131,34 @@ def main():
     setup_logger()
     print("Sit back and relax, TeleC2Dump script is running...")
 
+    # Queue for handling new bot tokens
+    queue = Queue()
+
     # Perform multithread process for each of the bot in config_file
-    threads = []
     with open(config_file, 'r') as file:
         lines = file.readlines()
         for i in lines:
             try:
                 botkey, attacker_chat_id, chatid = i.strip().split()
-                thread = threading.Thread(target=processing_botData, args=(botkey, attacker_chat_id, chatid, mode))
-                threads.append(thread)
-                thread.start()
+                queue.put((botkey, attacker_chat_id, chatid))
             except ValueError:
                 logging.error(f'Invalid format in config file: {i}')
                 pass
 
-    for thread in threads:
-        thread.join()
+    # Start a thread to process the new bot tokens
+    threading.Thread(target=process_new_bot_tokens, args=(queue, mode)).start()
+
+    # Allow dynamic addition of new bot tokens from your latest findings
+    while True:
+        print("Got new token ID? Enter the new bot token, attacker chat ID, and chat ID (separated by spaces), or 'exit' to quit:")
+        user_input = input().strip()
+        if user_input.lower() == 'exit':
+            break
+        try:
+            botkey, attacker_chat_id, chatid = user_input.split()
+            queue.put((botkey, attacker_chat_id, chatid))
+        except ValueError:
+            print("Invalid format! Please enter in the format: <BOT TOKEN ID> <ATTACKER CHAT ID> <YOUR CHAT ID>")
 
 if __name__ == "__main__":
     main()
