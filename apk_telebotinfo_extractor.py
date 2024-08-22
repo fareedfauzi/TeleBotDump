c9dd13515f34c4a71015ca28f604b71905cdb9aa
fareedfauzi <56353946+fareedfauzi@users.noreply.github.com>
2024-08-22 19:42:41 +0800

    Update and rename telebotinfo_extractor.py to apk_telebotinfo_extractor.py

--- apk_telebotinfo_extractor.py
+++ apk_telebotinfo_extractor.py
@@ -84,7 +84,7 @@ def main():
     check_jadx_installed()
 
     if len(sys.argv) != 2:
-        print('Usage: python Tele_botInfo_extractor.py /path/to/folder')
+        print('Usage: python apk_telebotinfo_extractor.py /path/to/folder')
         sys.exit(1)
     folder_path = sys.argv[1]
     if not os.path.isdir(folder_path):
